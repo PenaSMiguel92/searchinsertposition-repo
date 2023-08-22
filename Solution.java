05e055d400a4e3c8be6fda3ac917a4c17eba948f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-21 20:42:36 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,6 +1,28 @@
 public class Solution {
     public int searchInsert(int[] nums, int target) {
+        if (nums.length <= 1)
+            return 1;
+        int middleIndex = nums.length / 2;
+        if (target == nums[middleIndex])
+            return middleIndex;
+
+        if (target > nums[middleIndex]) {
+            int[] nextNums = new int[nums.length - middleIndex];
+            for (int i = 0; i < nums.length - middleIndex; i++) {
+                nextNums[i] = nums[i+middleIndex];
+            }
+            return middleIndex + searchInsert(nextNums, target);
+        }
+
+        if (target < nums[middleIndex]) {
+            int[] nextNums = new int[middleIndex];
+            for (int i = 0; i < middleIndex; i++) {
+                nextNums[i] = nums[i];
+            }
+            return searchInsert(nextNums, target);
+        }
 
         return 0;
     }
+    
 }
